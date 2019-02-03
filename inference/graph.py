991231bd9ebf2bfc315086a4ed2379eaf1c89498
Anton Petkov <anton.petkoff@gmail.com>
2019-02-03 14:07:22 +0200

    Extract inference system and inference graph tests

--- inference/graph.py
+++ inference/graph.py
@@ -1,8 +1,13 @@
-from inference import InferenceSystem
+from inference.system import InferenceSystem
+from inference.utils import get_consequent_variable_names
 
 class InferenceGraph:
     def __init__(self, definitions):
         self.definitions = definitions
+
+        consequent_variable_names = get_consequent_variable_names(self.definitions)
+        print('consequent_variable_names: {}'.format(consequent_variable_names))
+
         self.dependency_graph = InferenceGraph.__build_dependency_graph(self.definitions)
         print('dependency_graph: {}'.format(self.dependency_graph))
 
--- main.py
+++ main.py
@@ -5,20 +5,32 @@ import numpy as np
 from inference import InferenceSystem, InferenceGraph
 
 
-def main():
+def test_inference_system():
     with open('definitions_single.json') as definitions_json:
         definitions = json.load(definitions_json)
 
     inference_system = InferenceSystem(definitions)
-    # inference_system.visualize_rules()
+    inference_system.visualize_rules()
     result = inference_system.evaluate({
         'driving': 9.8,
         'journey_time': 2,
     })
     print('result: {}'.format(result))
-    # inference_system.visualize_result()
+    inference_system.visualize_result()
+
+
+def test_inference_graph():
+    with open('definitions_compound.json') as definitions_json:
+        definitions = json.load(definitions_json)
 
     inference_graph = InferenceGraph(definitions)
+    # TODO: evaluate
+    # TODO: visualize results
+
+
+def main():
+    # test_inference_system()
+    test_inference_graph()
 
 
 if __name__ == "__main__":

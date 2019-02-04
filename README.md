3406cc4879f277859dd7b77ce884b7f5fa75bb90
Anton Petkov <anton.petkoff@gmail.com>
2019-02-04 21:20:13 +0200

    Take a JSON file as argument in main.py

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ It is recommended to run the project in a virtual environment, e.g. `virtualenv`
 Run `pip install -r requirements.txt` to install the required Python libraries.
 
 Run `python main.py` to run the program.
+
+## Running
+
+Once you have defined a JSON file `my_definitions.json`
+(this name is given only as an example) with fuzzy variables,
+fuzzy rules and inputs, like the examples in `definitions/`,
+you can run the inference graph by executing:
+  `python main.py my_definitions.json`
+
+To run the example, execute:
+  `python main.py definitions/compound.json`
--- main.py
+++ main.py
@@ -1,45 +1,41 @@
+import sys
 import json
 import numpy as np
 from inference import InferenceSystem, InferenceGraph
 
 
-def test_inference_system():
-    with open('definitions/single.json') as definitions_json:
-        definitions = json.load(definitions_json)
-
+def run_inference_system(definitions):
     inference_system = InferenceSystem(definitions)
     inference_system.visualize_rules()
-    result = inference_system.evaluate({
-        'driving': 9.8,
-        'journey_time': 2,
-    })
+    result = inference_system.evaluate(definitions['inputs'])
     print('result: {}'.format(result))
     inference_system.visualize_result()
 
 
-def test_inference_graph():
-    with open('definitions/compound.json') as definitions_json:
-        definitions = json.load(definitions_json)
-
+def run_inference_graph(definitions):
     inference_graph = InferenceGraph(definitions)
     print('required inputs: {}'.format(inference_graph.get_required_inputs()))
 
-    result = inference_graph.evaluate(inputs={
-        'comfort': 3.4,
-        'journey_time': 1.3,
-        'average_speed': 61
-    })
+    result = inference_graph.evaluate(inputs=definitions['inputs'])
     print('result: {}'.format(result))
 
     inference_graph.visualize_result()
     input('Press ENTER to hide result visualization')
 
 
-def main():
-    # test_inference_system()
-    test_inference_graph()
+def main(definitions):
+    run_inference_graph(definitions)
 
 
 if __name__ == "__main__":
     np.random.seed(0)
-    main()
+
+    if len(sys.argv) < 2:
+        raise AttributeError("Missing JSON file argument")
+
+    definitions_file = sys.argv[1]
+
+    with open(definitions_file) as definitions_json:
+        definitions = json.load(definitions_json)
+
+    main(definitions)
